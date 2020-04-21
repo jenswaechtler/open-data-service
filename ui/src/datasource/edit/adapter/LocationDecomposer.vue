676d00d336fe0c0036e815fbdd9bcb58e74f3b97
Georg Schwarz <dev@georg-schwarz.com>
2020-04-21 16:10:51 +0200

    Add simple component to home

---
+++ ui/src/datasource/edit/adapter/LocationDecomposer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-text-field
+    v-model="url"
+    label="URL"
+    class="pl-7"
+    @keyup="formChanged"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import {Emit, Prop, PropSync, Watch} from 'vue-property-decorator'
+
+@Component({ })
+export default class LocationDecomposer extends Vue {
+  private url = "http://test.de/abc";
+
+  formChanged() {
+
+  }
+
+}
+</script>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="home">
     <h1>Welcome to Open-Data-Service UI</h1>
+
+    <v-card class="mx-auto">
+      <v-toolbar
+        dense
+        class="elevation-0"
+      >
+        <v-toolbar-title>
+          <span>Location Decompose: Jens Working Area</span>
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <location-decomposer />
+      </v-card-text>
+    </v-card>
   </div>
+
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
-@Component
+import LocationDecomposer from '../datasource/edit/adapter/LocationDecomposer.vue'
+
+@Component({
+  components: { LocationDecomposer }
+})
 export default class Home extends Vue {
 }
 </script>
